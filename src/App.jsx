8b45e7f8ef8f7fe0afca94310fc4be11bbb498f3
laxmi <laxmi.guptaa28@gmail.com>
2023-10-26 14:23:26 +0530

    home page compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import LoginSignup from './Pages/LoginSignup';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -28,6 +29,7 @@ function App() {
     <Route path="/login" element={<LoginSignup/>} />
 
     </Routes>
+    <Footer/>
     </BrowserRouter>
     </>
   )
