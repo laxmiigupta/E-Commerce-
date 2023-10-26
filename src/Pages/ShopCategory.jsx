97fc04116e79943c3b466786bea121681c89a5bd
laxmi <laxmi.guptaa28@gmail.com>
2023-10-26 17:38:37 +0530

    adding banner for mens & womens & kids but when we use context so getting error

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import {ShopContext} from '../Context/ShopContextProvider'
 
-export default function ShopCategory() {
+export default function ShopCategory(props) {
+  const{all_product} = useContext(ShopContext);
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+    </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// import { ShopContextProvider } from './Context/ShopContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  
+    // <ShopContextProvider>
+      <App />
+      // </ShopContextProvider>
+  
+  );
+
+
+  // import React from 'react'
+  // import ReactDOM from 'react-dom';
+  // import App from './App.jsx';
+  // import './index.css';
+  // import { ShopContextProvider } from './Context/ShopContextProvider.jsx';
+  
+  // ReactDOM.render(
+  //   <ShopContextProvider>
+  //     <App />
+  //   </ShopContextProvider>,
+  //   document.getElementById('root')
+  //   );
