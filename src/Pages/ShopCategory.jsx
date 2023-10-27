7703996154eb9f6a9a5c2fdec00ef4e57fbd8fd9
Laxmi Gupta <laxmi.guptaa28@gmail.com>
2023-10-27 11:40:28 +0530

    fixed use Context error

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -3,7 +3,8 @@ import './CSS/ShopCategory.css'
 import {ShopContext} from '../Context/ShopContextProvider'
 
 export default function ShopCategory(props) {
-  const{all_product} = useContext(ShopContext);
+  const all_product = useContext(ShopContext);
+  console.log(all_product);
   return (
     <div className='shop-category'>
       <img src={props.banner} alt="" />
